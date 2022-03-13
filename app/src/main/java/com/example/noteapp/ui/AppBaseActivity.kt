f57e489f2d82d195688ad34bc44c8906148bd51a
Abu Raihan Rony <ronyaburaihan@gmail.com>
2022-03-13 12:46:27 +0600

    Some designs has been updated

--- app/src/main/java/com/example/noteapp/ui/AppBaseActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/AppBaseActivity.kt
@@ -1,7 +1,5 @@
 package com.example.noteapp.ui
 
-import android.app.Dialog
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -11,7 +9,6 @@ import com.example.noteapp.R
 
 open class AppBaseActivity : AppCompatActivity() {
 
-    private var progressDialog: Dialog? = null
     private val TAG: String = "AppBaseActivity"
 
     fun setToolbarWithoutBackButton(mToolbar: Toolbar) {
@@ -24,39 +21,11 @@ open class AppBaseActivity : AppCompatActivity() {
         mToolbar.setNavigationIcon(R.drawable.ic_back)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (progressDialog == null) {
-            progressDialog = Dialog(this)
-            progressDialog?.window?.setBackgroundDrawable(ColorDrawable(0))
-            progressDialog?.setContentView(R.layout.layout_progress_dialog)
-        }
-
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> onBackPressed()
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    fun showProgress(show: Boolean) {
-        when {
-            show -> {
-                if (!isFinishing && !progressDialog!!.isShowing) {
-                    progressDialog?.setCanceledOnTouchOutside(false)
-                    progressDialog?.show()
-                }
-            }
-            else -> try {
-                if (progressDialog?.isShowing!! && !isFinishing) {
-                    progressDialog?.dismiss()
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
     }
 
     override fun onStart() {
