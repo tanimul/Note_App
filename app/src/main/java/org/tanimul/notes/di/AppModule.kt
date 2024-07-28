c8d804b8ce88b3fa2bddcfa05ee348f52cd839ed
tanimul <tanimulislam.fuad@gmail.com>
2024-07-28 23:31:58 +0600

    App module updated with editor & note repository.

--- app/src/main/java/org/tanimul/notes/di/AppModule.kt
+++ app/src/main/java/org/tanimul/notes/di/AppModule.kt
@@ -7,15 +7,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.tanimul.notes.data.database.NoteDatabase
-import org.tanimul.notes.ui.fragments.editor.data.AddNoteRepositoryImpl
-import org.tanimul.notes.ui.fragments.editor.data.DeleteNoteRepositoryImpl
-import org.tanimul.notes.ui.fragments.editor.data.UpdateNoteRepositoryImpl
-import org.tanimul.notes.ui.fragments.editor.domain.repository.AddNoteRepository
-import org.tanimul.notes.ui.fragments.editor.domain.repository.DeleteNoteRepository
-import org.tanimul.notes.ui.fragments.editor.domain.repository.UpdateNoteRepository
-import org.tanimul.notes.ui.fragments.notes.data.DeleteAllNoteRepositoryImpl
+import org.tanimul.notes.ui.fragments.editor.data.EditorRepositoryImpl
+import org.tanimul.notes.ui.fragments.editor.domain.repository.EditorRepository
 import org.tanimul.notes.ui.fragments.notes.data.NotesRepositoryImpl
-import org.tanimul.notes.ui.fragments.notes.domain.repository.DeleteAllNoteRepository
 import org.tanimul.notes.ui.fragments.notes.domain.repository.NotesRepository
 
 @Module
@@ -29,26 +23,11 @@ object AppModule {
     }
 
     @Provides
-    fun provideAddNoteRepository(addNoteRepositoryImpl: AddNoteRepositoryImpl): AddNoteRepository =
-        addNoteRepositoryImpl
-
-    @Provides
-    fun provideUpdateNoteRepository(updateNoteRepositoryImpl: UpdateNoteRepositoryImpl): UpdateNoteRepository =
-        updateNoteRepositoryImpl
-
-    @Provides
-    fun provideDeleteNoteRepository(deleteNoteRepositoryImpl: DeleteNoteRepositoryImpl): DeleteNoteRepository =
-        deleteNoteRepositoryImpl
-
-    @Provides
     fun provideNotesRepository(notesRepositoryImpl: NotesRepositoryImpl): NotesRepository =
         notesRepositoryImpl
 
-/*    @Provides
-    fun provideDeleteNoteRepository(deleteNoteRepositoryImpl: org.tanimul.notes.ui.fragments.notes.data.DeleteNoteRepositoryImpl): org.tanimul.notes.ui.fragments.notes.domain.repository.DeleteNoteRepository =
-        deleteNoteRepositoryImpl*/
-
     @Provides
-    fun provideDeleteAllNoteRepository(deleteAllNoteRepositoryImpl: DeleteAllNoteRepositoryImpl): DeleteAllNoteRepository =
-        deleteAllNoteRepositoryImpl
+    fun provideEditorRepository(editorRepositoryImpl: EditorRepositoryImpl): EditorRepository =
+        editorRepositoryImpl
+
 }
